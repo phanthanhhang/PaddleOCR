34e1c79618da0178c7fcc5b034a883826889046c
phanthanhhang <phanthanhhang.lhp@gmail.com>
2025-07-31 19:00:00 +0700

    export command

---
+++ export_model.sh
@@ -0,0 +1,5 @@
+python tools/export_model.py \
+-c output/germen_custom_PP-OCRv5_server_rec_19072025/config.yml \
+-o Global.pretrained_model=output/germen_custom_PP-OCRv5_server_rec_19072025/best_accuracy \
+ Global.save_inference_dir=output/export_model_german_custom_PP-OCRv5_server_rec_19072025
+# -c configs/rec/PP-OCRv5/german_custom_PP_OCRv5_server_rec.yaml \
