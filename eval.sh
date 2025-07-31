9a8bd9d8ccfd7b32b5b073c29da04741b4f4e767
phanthanhhang <phanthanhhang.lhp@gmail.com>
2025-07-31 18:59:37 +0700

    eval command

---
+++ eval.sh
@@ -0,0 +1,3 @@
+python tools/eval.py \
+-c configs/rec/PP-OCRv5/german_custom_PP_OCRv5_server_rec.yaml \
+-o Global.pretrained_model=output/germen_custom_PP-OCRv5_server_rec_11072025/iter_epoch_30.pdparams 
